ae423938af5686598f085357e9b9da10b247ac34
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2019-04-08 20:37:12 -0400

    Add contents of tables.py to db.py

--- app/db.py
+++ app/db.py
@@ -2,15 +2,59 @@ import os
 import sqlite3
 
 from app.connection import SQLiteConnection
-from app.tables import (
-	sqlite_create_store_table, sqlite_create_transaction_table,
-	sql_clear_store_table, sql_clear_transaction_table,
-	sqlite_list_tables
-)
 from app.settings import DB_URL
 from app.utils import execute_sql
 
 
+# SQLite specific statements
+sqlite_create_store_table = """ CREATE TABLE IF NOT EXISTS store (
+						id INTEGER PRIMARY KEY AUTOINCREMENT,
+						name VARCHAR(50) NOT NULL UNIQUE
+						); """
+
+sqlite_create_purchase_table = """ CREATE TABLE IF NOT EXISTS purchase (
+							id INTEGER PRIMARY KEY AUTOINCREMENT,
+							purchase_date TEXT NOT NULL,
+							total INTEGER NOT NULL,
+							description TEXT NOT NULL,
+							store_id INTEGER NOT NULL,
+							FOREIGN KEY (store_id) REFERENCES store(id),
+							UNIQUE(purchase_date, total, description, store_id)
+							); """
+
+sqlite_list_tables = """ SELECT name FROM sqlite_master WHERE type='table'; """
+
+
+# Postgres specific statements
+postgres_create_store_table = """ CREATE TABLE store (
+						id SERIAL PRIMARY KEY,
+						name VARCHAR(50) NOT NULL UNIQUE
+						); """
+
+postgres_create_purchase_table = """ CREATE TABLE purchase (
+							id SERIAL PRIMARY KEY,
+							purchase_date DATE NOT NULL,
+							total INT NOT NULL,
+							description TEXT NOT NULL,
+							store_id INTEGER NOT NULL REFERENCES store(id),
+							UNIQUE(purchase_date, total, description, store_id)
+							); """
+
+postgres_list_tables = """ SELECT table_name FROM information_schema.tables
+   						WHERE table_schema = 'public'; """
+
+
+# SQL general statements
+sql_clear_store_table = """ DELETE FROM store; """
+
+sql_clear_purchase_table = """ DELETE FROM purchase; """
+
+sql_delete_store_table = """ DROP TABLE store; """
+
+sql_delete_purchase_table = """ DROP TABLE purchase; """
+
+
+# Useful db methods
 def setup_db(conn):
 	"""
 	Set up the sqlite database with store and puchase tables.
