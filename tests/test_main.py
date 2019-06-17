d782b97a3de74c509c1752b9a0d8fba1491fb58a
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2019-06-17 13:35:00 -0400

    Partial test groc list

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,10 @@
 from unittest import mock
 
 from click.testing import CliRunner
+from prettytable import from_db_cursor
 
 from app.main import groc_entrypoint as groc_cli
-from app import exceptions, groc
+from app import db, exceptions, groc
 
 
 def test_hi():
@@ -66,3 +67,87 @@ def test_reset(groc_clear, groc_purchase_count, groc_connection, groc_db_url):
                              'Database reset successful.\n')
 
 
+@mock.patch('app.main.Groc._get_db_url')
+@mock.patch('app.main.Groc._get_connection')
+@mock.patch('app.main.Groc.select_purchase_count', return_value=0, autospec=True)
+def test_list_limit_no_purchases(
+        groc_purchase_count, groc_connection, groc_db_url):
+    groc.Groc()
+
+    runner = CliRunner()
+    result = runner.invoke(groc_cli, ['list', '--verbose'])
+    assert result.exit_code == 0
+    assert result.output == 'No purchase entries available. You should add some!\nSee groc add --help to add purchases.\n'
+
+
+@mock.patch('app.main.Groc._get_db_url')
+@mock.patch('app.main.Groc._get_connection')
+@mock.patch('app.main.Groc.select_purchase_count', return_value=9, autospec=True)
+@mock.patch('app.main.Groc.list_purchases_limit', autospec=True)
+def test_list_limit_more_than_100(
+        groc_purchase_limit, groc_purchase_count, groc_connection,
+        groc_db_url, connection_function_scope, stores_and_purchases_function_scope):
+    groc_connection.return_value = connection_function_scope
+    g = groc.Groc()
+
+    # Make table
+    table = from_db_cursor(g.list_purchases_limit(101))
+    table.title = f'Last 50 purchase(s)'
+    table.align['store'] = 'r'
+    table.align['total'] = 'r'
+    table.align['description'] = 'l'
+
+    runner = CliRunner()
+    result = runner.invoke(groc_cli, ['list', '--verbose'])
+    assert result.exit_code == 0
+    assert result.output == f'{table.get_string()}\n'
+
+
+@mock.patch('app.main.Groc._get_db_url')
+@mock.patch('app.main.Groc._get_connection')
+@mock.patch('app.main.Groc.select_purchase_count', return_value=9, autospec=True)
+@mock.patch('app.main.Groc.list_purchases_limit', autospec=True)
+def test_list_limit_default(
+        groc_purchase_limit, groc_purchase_count, groc_connection, 
+        groc_db_url, connection_function_scope, stores_and_purchases_function_scope):
+    groc_connection.return_value = connection_function_scope
+    g = groc.Groc()
+
+    # Make table
+    table = from_db_cursor(g.list_purchases_limit(50))
+    table.title = f'Last 50 purchase(s)'
+    table.align['store'] = 'r'
+    table.align['total'] = 'r'
+    table.align['description'] = 'l'
+
+    runner = CliRunner()
+    result = runner.invoke(groc_cli, ['list', '--verbose'])
+    assert result.exit_code == 0
+    assert result.output == f'{table.get_string()}\n'
+
+
+@mock.patch('app.main.Groc._get_db_url')
+@mock.patch('app.main.Groc._get_connection')
+@mock.patch('app.main.Groc.select_purchase_count', return_value=9, autospec=True)
+def test_list_limit_default_no_verbose(
+    groc_purchase_count, groc_connection,
+    groc_db_url, connection_function_scope,
+    stores_and_purchases_function_scope
+):
+    groc_connection.return_value = connection_function_scope
+    g = groc.Groc()
+
+    # Make table
+    purchases = g.list_purchases_limit(50)
+    table = from_db_cursor(purchases)
+    table.title = f'Last 50 purchase(s)'
+    table.align['store'] = 'r'
+    table.align['total'] = 'r'
+    table.align['description'] = 'l'
+    field_names = [name for name in table.field_names]
+    field_names.remove('id')
+
+    runner = CliRunner()
+    result = runner.invoke(groc_cli, ['list'])
+    assert result.exit_code == 0
+    assert result.output == f'{table.get_string(fields=field_names)}\n'
